782a5af03bcf5ce9a7cd914f4a806cb41a965843
mahmoudbadr2 <mahmoudb731@gmail.com>
2025-12-14 17:32:25 +0200

    add title and subtitle for details

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -9,6 +9,7 @@ abstract class Styles {
   static const textStyle30 = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.w600,
+    fontFamily: 'Spectral',
   );
 
   static const textStyle20 = TextStyle(
